27c834c80ffbb3b47b84db18971da790cab8e7d2
Ernest Correale <ecorreale@me.com>
2020-06-01 20:10:16 -0400

    Added comments and repeatable questions

--- utils/questions.js
+++ utils/questions.js
@@ -1,5 +1,8 @@
 const inquirer = require('inquirer')
 
+// ####################################################
+//          Username and repo Name Questions
+// ####################################################
 exports.AcctRepo = [{
 
     type: "input",
@@ -16,86 +19,104 @@ exports.AcctRepo = [{
 ];
 
 
+
+
+// ####################################################
+//                 Badge Questions
+// ####################################################
+exports.IsBadgeNeeded = [{
+  type: "confirm",
+  name: "IsBadgeNeeded",
+  message: "Would you like to include badge(s)?  (Enter for YES)"
+}];
+
 exports.Badges = [{
   type: "checkbox",
-  name: "prjBadges",
+  name: "badgesSelected",
   message: "Select from available badges.",
+  // Choice Name Format [BadgeKey]:[Badge Description]
   choices: [
     new inquirer.Separator("======= Platform & Version Support ========\n"),
     {
-      name: "Node: Latest Node version badge"
+      name: "1: Latest Node version badge"
     },
     {
-      name: "GitHub: Last commit badge"
+      name: "2: GitHub, last commit badge"
     },
     {
-      name: "Badges: We don't need no stink'n badges!"
+      name: "3: We don't need no stink'n badges!"
     }
   ]
 }];
 
-
+// ####################################################
+//              Contributors Questions
+// ####################################################
 exports.AnyContributors = [{
   type: "confirm",
-  name: "HasContrib",
-  message: "Do you have any GitHub Contributors (just hit enter for YES)?"
+  name: "HasContributor",
+  message: "Do you have any GitHub Contributors? (Enter for YES)"
 }];
 
-exports.GetContributors = [{
-    type: "input",
-    name: "Contributor",
-    message: "\nEnter Contributor's GitHub username."
 
+exports.Contributors = [{
+    type: "input",
+    name: "value",
+    message: "Enter Contributor's GitHub username."
   },
 
   {
     // Ask if more names need to be added
     type: "confirm",
-    name: "addContrib",
-    message: "Add another contributor (just hit enter for YES)?"
+    name: "askAgain",
+    message: "Add another contributor (Enter for YES)?"
   }
 ];
 
+
+// ####################################################
+//             Document Content Questions
+// ####################################################
 exports.Sections = [{
     type: "input",
-    name: "prjTitle ",
+    name: "Title ",
     message: "What is the title for your project ? "
   },
 
   {
     type: "input",
-    name: "prjDesc ",
+    name: "Description ",
     message: "Project Description ? "
   },
 
   {
     type: "input",
-    name: "prjInstall",
+    name: "Install",
     message: "What are the steps required to install your project ? "
   },
 
   {
     type: "input",
-    name: "prjUsage",
+    name: "Usage",
     message: "Usage\nProvide instructions and examples for use. "
   },
 
   {
     type: "input",
-    name: "prjTest",
+    name: "Test",
     message: "\nSection: Testing\nCan you provide test instructions"
   },
 
   {
     type: "list",
-    name: "prjLicense",
+    name: "License",
     message: "Select a license for this project",
     choices: ["MIT License (MIT)", "GNU General Public License v3.0 (GNU GPLv3)", "Apache License 2.0", "The Unlicense", "Other"]
   },
 
   {
     type: "input",
-    name: "prjContact",
+    name: "Contact",
     message: "Can you provide an email address for the Questions section?"
   }
 ];
