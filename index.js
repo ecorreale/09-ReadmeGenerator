348ec44254c9513aed11e9af00dec7a4cb80296e
Ernest Correale <ecorreale@me.com>
2020-06-08 21:03:35 -0400

    Changed to Contributing

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const inquirer = require('inquirer')
 const path = require('path');
 const axios = require('axios')
 const fs = require('fs')
+
 const Questions = require("./utils/questions")
- var badges = {}
+const MarkDown = require("./utils/generateMarkdown")
+
+var docData = {}
 
 
 function writeToFile(fileName, data) {
@@ -12,26 +15,17 @@ function writeToFile(fileName, data) {
         console.log("File Updated");
     });
 }
-//  https://github.com/ecorreale/09-ReadmeGenerator
-// https://api.github.com/users/ecorreale
-// ecorreale
-
-// ToDo:Add question for Contributors
-
-//  ToDo: Add confirm for Badges
-
-//ToDo: ![node-current (scoped)](https://img.shields.io/node/v/@stdlib/stdlib?style=plastic)
-
 
 function init() {
     const apiUrl = "https://api.github.com"
 
 
     inquirer.prompt(Questions.Repo).then(answers => {
+
         const profileApi = apiUrl + "/users/" + answers.userName
 
         const repoName = answers.repoName
-        const repoApi = profileApi + "/" + repoName.replace(/ /g, "-")
+        const repoApi = apiUrl + "/repos/" + answers.userName + "/" + repoName.replace(/ /g, "-")
 
         axios.get(profileApi).then(results => {
                 if (!results) {
@@ -39,58 +33,76 @@ function init() {
                     throw "Please confoirm the supplied repo URL is correct and try again"
                 }
 
-                const profileImageUrl = results.data.avatar_url
-                const emailAddr = results.data.email
+                docData["UserName"] = answers.userName
+                docData["RepoName"] = repoName
+                docData["ProfileImage"] = results.data.avatar_url
+                docData["ProfileEmail"] = results.data.email
 
             })
             .then(() => {
-                console.log("\n---------------Then--------------------\n")
-                getBadges()
+
+                Badges()
             })
     })
 }
 
-function getBadges() {
-    var badgeKey = ""
+function Badges() {
+
     inquirer.prompt(Questions.IsBadgeNeeded).then(answers => {
 
-        if (answers.IsBadgeNeeded) {
-
-            inquirer.prompt(Questions.Badges).then(answers => {
-
-                (answers.badgesSelected).map(item => {
-                    badgeKey = item.split(":")[0]
-                    badgeValue = item.split(":")[1]
-
-                    badges[badgeKey] = badgeValue
-                })
-
-            }).then(() => {
-                console.log("\n---------------Then--------------------\n")
-                contributors()
-            })
-        }
-    })
-}
-
-function contributors() {
-    inquirer.prompt(Questions.AnyContributors).then(answers => {
-        if (answers.HasContributor) {
-            repeatingQuestion(Questions.Contributors)
-        }
-    })
-}
-
-
-function repeatingQuestion(Question) {
-    inquirer.prompt(Question).then(answers => {
-        output.push(answers.value);
-        if (answers.askAgain) {
-            repeatingQuestion(Question);
+        if (answers.IsNeeded == true) {
+            GetBadges()
         } else {
-            console.log('List: ', output.join(', '));
+            Contributors()
         }
-    });
+    })
 }
+
+function GetBadges() {
+
+BadgeTable[0] = "![node-current (scoped)](https://img.shields.io/node/v/@stdlib/stdlib?style=plastic)"
+BadgeTable[1] = "![GitHub Last Commit](https://img.shields.io/github/last-commit/" + docData.UserName + "/" + docData.RepoName +"?style=plastic)"
+BadgeTable[2] = "![GitHub](https://img.shields.io/github/license/"+ docData.UserName + "/" + docData.RepoName + "?style=plastic)"
+    var badges = {}
+
+    inquirer.prompt(Questions.Badges).then(answers => {
+
+        (answers.Choices).map(item => {
+
+            var selection = item.split(":")[0]
+            var index = selection -1
+            badges += BadgeTable[index]
+        })
+        docData["Badges"] = badges;
+    }).then(() => {
+
+        DocumentBody()
+    })
+}
+
+
+
+
+function DocumentBody() {
+    console.log("Document Body")
+
+    inquirer.prompt(Questions.Document).then(answers => {
+        docData["Title"] = answers.Title
+        docData["Description"] = answers.Description
+        docData["Install"] = answers.Install
+        docData["Usage"] = answers.Usage
+        docData["Contributing"] = answers.Contributing
+        docData["Testing"] = answers.Testing
+        docData["License"] = answers.License
+        docData["Contact"] = answers.Contact
+
+    }).then(() => {
+        MarkDown(docData)
+    })
+}
+
+
+
+
 
 init();
