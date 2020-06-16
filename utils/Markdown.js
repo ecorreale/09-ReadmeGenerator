abb82e93d5990ceb9a5713166a1475440613a348
Ernest Correale <ecorreale@me.com>
2020-06-15 23:07:07 -0400

    ?Minor Bug Fix

--- utils/Markdown.js
+++ utils/Markdown.js
@@ -3,19 +3,33 @@ function generateMarkdown(data) {
     // Text Array
     var Markdown = []
     const imageHeight = 135
-    const detailBoxHeight = imageHeight + 10
 
     Markdown.push("# " + data.DocumentTitle)
     Markdown.push("")
 
     Markdown.push("<img src='" + data.ProfileImage +
         "' height='" + imageHeight +"' style='float:left;margin:10px'>")
-    Markdown.push("")
+
+        Markdown.push("")
+
+        if (data.Author) {
+            Markdown.push("**Author:** " + data.Author)
+            Markdown.push("<br/>")
+        }
+
+        if (data.License) {
+            Markdown.push("**License:** " + data.License)
+            Markdown.push("<br/>")
+        }
+
+    // Print out each selected badge
+    data.Badges.forEach(function (badge, index) {
+        Markdown.push(badge)
+    })
+
 
     // Table of Contents Right-Justified on page
     if (data.ShowToc) {
-
-        Markdown.push("<div style='float:right;margin-right:35px;;margin-top:-20px'>\n")
 
         Markdown.push("#### Table of Contents\n")
 
@@ -23,6 +37,9 @@ function generateMarkdown(data) {
 
         if (data.DescriptionText) {
             Markdown.push("  - [Description](#Description)")
+        }
+        if (data.Install) {
+            Markdown.push("  - [Installation](#Installation)")
         }
         if (data.Usage) {
             Markdown.push("  - [Usage](#Usage)")
@@ -32,42 +49,17 @@ function generateMarkdown(data) {
         }
 
         if (data.DescriptionText) {
-            Markdown.push("  - [Comments](#Comments)")
+            Markdown.push("  - [Contributing](#Contributing)")
         }
-        if (data.Install) {
-            Markdown.push("  - [Installation](#Installation)")
-        }
-
-        Markdown.push(" ")
-        Markdown.push("</div>")
     }
 
 
 
-    // Create property details box
-    Markdown.push("<div style='min-height:" + detailBoxHeight + "px;'\n")
-
-    if (data.Author) {
-        Markdown.push("**Author:** " + data.Author)
-        Markdown.push("<br/>")
-    }
-
-    if (data.License) {
-        Markdown.push("**License:** " + data.License)
-        Markdown.push("<br/>")
-    }
     if (data.SummaryText) {
-        Markdown.push("**Summary:** " + data.SummaryText)
+        Markdown.push("# Summary")
+        Markdown.push(data.SummaryText)
+        Markdown.push(" ")
     }
-
-    Markdown.push("\n</div>\n")
-
-
-    // Print out each selected badge
-    data.Badges.forEach(function (badge, index) {
-        Markdown.push(badge)
-    })
-
 
     if (data.DescriptionText) {
         Markdown.push("# Description")
@@ -82,7 +74,7 @@ function generateMarkdown(data) {
 
 
     if (data.Install) {
-        Markdown.push("# Installation Instructions")
+        Markdown.push("# Installation")
         Markdown.push(data.Install)
         Markdown.push(" ")
     }
